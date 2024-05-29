2b8873af7ebbd853268119aed9703eb4b3a19c9e
adeyemoadeniran <niranadeniran1@gmail.com>
2024-05-29 12:26:19 +0100

    Update

--- script.js
+++ script.js
@@ -3,9 +3,31 @@ const convertBtn = document.getElementById('convert-btn');
 const result = document.getElementById('result');
 
 function checkUserInput() {
-  console.log(numberInput.value);
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    alert('Please provide a decimal number');
+    return;
+  }
+
+  decimalToBinary(parseInt(numberInput.value));
+
+  numberInput.value = '';
 }
 
 convertBtn.addEventListener('click', checkUserInput);
 
-numberInput.addEventListener('keydown', () => {});
+numberInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    checkUserInput();
+  }
+});
+
+function decimalToBinary(input) {
+  const inputs = [];
+  const quotients = [];
+  const remainders = [];
+
+  while (input > 0) {
+    const quotient = Math.floor(input / 2);
+    input = quotient;
+  }
+}
