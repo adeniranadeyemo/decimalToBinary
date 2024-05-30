39f2759978f08f571c544f6c9ce9f7228ec3d542
adeyemoadeniran <niranadeniran1@gmail.com>
2024-05-30 20:59:40 +0100

    Added the binary functionality

--- script.js
+++ script.js
@@ -26,8 +26,21 @@ function decimalToBinary(input) {
   const quotients = [];
   const remainders = [];
 
+  if (input === 0) {
+    result.innerText = '0';
+    return;
+  }
+
   while (input > 0) {
     const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
+
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
+
     input = quotient;
   }
+
+  result.innerText = remainders.reverse().join('');
 }
