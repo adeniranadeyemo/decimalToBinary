bf2634c27cea6be51b9c249d193aa10082b3ef7f
adeyemoadeniran <niranadeniran1@gmail.com>
2024-05-31 12:11:00 +0100

    Made the decimal to binary function recursive and started an animation addition.

---
+++ previous.js
@@ -0,0 +1,37 @@
+// function decimalToBinary(input) {
+// const inputs = [];
+// const quotients = [];
+// const remainders = [];
+
+// if (input === 0) {
+//   result.innerText = '0';
+//   return;
+// }
+
+// while (input > 0) {
+//   const quotient = Math.floor(input / 2);
+
+//   const remainder = input % 2;
+//   inputs.push(input);
+//   quotients.push(quotient);
+//   remainders.push(remainder);
+//   input = quotient;
+// }
+// result.innerText = remainders.reverse().join('');
+// }
+
+///////////////////////
+// function decimalToBinary(input) {
+//   let binary = '';
+
+//   if (input === 0) {
+//     binary = '0';
+//   }
+
+//   while (input > 0) {
+//     binary = (input % 2) + binary;
+//     input = Math.floor(input / 2);
+//   }
+
+//   result.innerText = binary;
+// }
--- script.js
+++ script.js
@@ -1,14 +1,52 @@
 const numberInput = document.getElementById('number-input');
 const convertBtn = document.getElementById('convert-btn');
 const result = document.getElementById('result');
+const animationContainer = document.getElementById('animation-container');
+
+const animationData = [
+  {
+    inputVal: 5,
+    marginTop: 300,
+    addElDelay: 1000,
+  },
+  {
+    inputVal: 2,
+    marginTop: -200,
+    addElDelay: 1500,
+  },
+  {
+    inputVal: 1,
+    marginTop: -200,
+    addElDelay: 2000,
+  },
+];
+
+function decimalToBinary(input) {
+  if (input === 0 || input === 1) {
+    return String(input);
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+  }
+}
+
+const showAnimation = () => {
+  result.innerText = 'Call Stack Animation';
+};
 
 function checkUserInput() {
-  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+  const inputInt = parseInt(numberInput.value);
+
+  if (!numberInput.value || isNaN(inputInt)) {
     alert('Please provide a decimal number');
     return;
   }
 
-  decimalToBinary(parseInt(numberInput.value));
+  if (inputInt === 5) {
+    showAnimation();
+    return;
+  }
+
+  result.textContent = decimalToBinary(inputInt);
 
   numberInput.value = '';
 }
@@ -21,26 +59,17 @@ numberInput.addEventListener('keydown', (e) => {
   }
 });
 
-function decimalToBinary(input) {
-  const inputs = [];
-  const quotients = [];
-  const remainders = [];
+///////////////////////////////////
+// Recursive function example
+// function countDownAndUp(number) {
+//   console.log(number);
+//   if (number === 0) {
+//     console.log('Reached base case');
+//     return;
+//   } else {
+//     countDownAndUp(number - 1);
+//     console.log(number);
+//   }
+// }
 
-  if (input === 0) {
-    result.innerText = '0';
-    return;
-  }
-
-  while (input > 0) {
-    const quotient = Math.floor(input / 2);
-    const remainder = input % 2;
-
-    inputs.push(input);
-    quotients.push(quotient);
-    remainders.push(remainder);
-
-    input = quotient;
-  }
-
-  result.innerText = remainders.reverse().join('');
-}
+// countDownAndUp(3);
